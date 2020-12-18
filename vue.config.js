238f7ebc8a82f16e32ac01815791b4814799c516
chenmoonmo <chenmoonmo@gmail.com>
2020-12-18 17:04:00 +0800

    配置前端跨域

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
   devServer: {
     proxy: {//配置跨域
       '/api': {
-        target: '',//这里后台的地址模拟的;应该填写你们真实的后台接口
+        target: 'http://192.169.0.1:8080',//这里后台的地址模拟的;应该填写你们真实的后台接口
         ws: true,
         changOrigin: true,//允许跨域
         pathRewrite: {
